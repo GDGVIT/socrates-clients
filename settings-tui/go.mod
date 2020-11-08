62b4237a2080407aa596c5a66808163f7364946e
SaurusXI <vermashantanu@hotmail.com>
2020-11-08 14:58:23 +0530

    remove settings-tui folder

--- settings-tui/go.mod
+++
@@ -1,8 +0,0 @@
-module settings-tui
-
-go 1.15
-
-require (
-	github.com/gdamore/tcell v1.4.0
-	github.com/rivo/tview v0.0.0-20200915114512-42866ecf6ca6
-)
--- settings-tui/tui/screen/elements/init.go
+++
@@ -1,7 +0,0 @@
-package elements
-
-const (
-	BtnBoxTotalCols = 8
-	DomainFieldText = "Add a domain:"
-	FreqFieldText = "Number of papers per day (max 5):"
-)
