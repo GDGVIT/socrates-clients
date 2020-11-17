3b046fb2f4aba63341818216281615a87f2f822d
SaurusXI <vermashantanu@hotmail.com>
2020-11-17 18:52:46 +0530

    Switch to logrus for logging

--- settings/api/api.go
+++ settings/api/api.go
@@ -7,7 +7,7 @@ import (
 	"api/controller"
 	"api/model"
 	"api/routes"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"os"
 	"path"
 )
@@ -22,23 +22,30 @@ func initConfig() {
 	}
 }
 
+func initLogging(logsPath string) {
+	log.SetFormatter(&log.JSONFormatter{PrettyPrint: true})
+	file, err := os.OpenFile(logsPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.SetOutput(file)
+}
+
 func main() {
 	// Setup Config
 	initConfig()
 
 	// Setup logging
 	logsPath := path.Join(viper.GetString("API_LOG_DIR"), "socrates-api-logs.txt")
-	file, err := os.OpenFile(logsPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.SetOutput(file)
+	initLogging(logsPath)
 
 	// Start app
 	router := httprouter.New()
 	model := model.New(viper.GetString("API_MODEL_DIR"))
 	ctrl := controller.New(model)
 	routes.MakeRoutes(router, ctrl)
+
+	log.Printf("Socrates API serving on PORT %s", viper.GetString("API_PORT"))
 
 	log.Fatal(http.ListenAndServe(":" + viper.GetString("API_PORT"), router))
 }
